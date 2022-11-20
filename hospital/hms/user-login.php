14248d9345c77608561446a27fa0fff35bc5c1ba
duban4 <75090589+duban4@users.noreply.github.com>
2022-11-20 07:27:55 -0500

    hospital

---
+++ hospital/hms/user-login.php
@@ -0,0 +1,132 @@
+<?php
+session_start();
+error_reporting(0);
+include("include/config.php");
+if(isset($_POST['submit']))
+{
+$ret=mysqli_query($con,"SELECT * FROM users WHERE email='".$_POST['username']."' and password='".md5($_POST['password'])."'");
+$num=mysqli_fetch_array($ret);
+if($num>0)
+{
+$extra="dashboard.php";//
+$_SESSION['login']=$_POST['username'];
+$_SESSION['id']=$num['id'];
+$host=$_SERVER['HTTP_HOST'];
+$uip=$_SERVER['REMOTE_ADDR'];
+$status=1;
+// For stroing log if user login successfull
+$log=mysqli_query($con,"insert into userlog(uid,username,userip,status) values('".$_SESSION['id']."','".$_SESSION['login']."','$uip','$status')");
+$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
+exit();
+}
+else
+{
+	// For stroing log if user login unsuccessfull
+$_SESSION['login']=$_POST['username'];
+$uip=$_SERVER['REMOTE_ADDR'];
+$status=0;
+mysqli_query($con,"insert into userlog(username,userip,status) values('".$_SESSION['login']."','$uip','$status')");
+$_SESSION['errmsg']="Usuario invalido o contraseña";
+$extra="user-login.php";
+$host  = $_SERVER['HTTP_HOST'];
+$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
+exit();
+}
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>User-Login</title>
+
+		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+		<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+		<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+		<link rel="stylesheet" href="assets/css/styles.css">
+		<link rel="stylesheet" href="assets/css/plugins.css">
+		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+	</head>
+	<body class="login mt-4" style>
+		<div class="row">
+			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
+				<div class="logo margin-top-30">
+				<h2 class="text-center  s-1">Login</h2>
+				</div>
+
+				<div class="box-login">
+					<form class="form-login" method="post">
+						<fieldset>
+							<legend>
+								Inicia sesion
+							</legend>
+							<p>
+								Por favor ingrese su nombre y contraseña<br />
+								<span style="color:red;"><?php echo $_SESSION['errmsg']; ?><?php echo $_SESSION['errmsg']="";?></span>
+							</p>
+							<div class="form-group">
+								<span class="input-icon">
+									<input type="text" class="form-control" name="username" placeholder="Nombre">
+									<i class="fa fa-user"></i>
+								 </span>
+							</div>
+							<div class="form-group form-actions">
+								<span class="input-icon">
+									<input type="password" class="form-control password" name="password" placeholder="Contraseña">
+									<i class="fa fa-lock"></i>
+									 </span><a href="forgot-password.php">
+									Has olvidado tu contraseña ?
+								</a>
+							</div>
+							<div class="form-actions">
+
+								<button type="submit" class="btn btn-primary pull-right" name="submit">
+									Entrar <i class="fa fa-arrow-circle-right"></i>
+								</button>
+							</div>
+							<div class="new-account">
+								Aun no tienes unacuenta?
+								<a href="registration.php">
+									Crear Cuenta
+								</a>
+							</div>
+						</fieldset>
+					</form>
+
+					<div class="copyright">
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> </span>. <span></span>
+					</div>
+
+				</div>
+
+			</div>
+		</div>
+		<script src="vendor/jquery/jquery.min.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+		<script src="vendor/modernizr/modernizr.js"></script>
+		<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
+		<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+		<script src="vendor/switchery/switchery.min.js"></script>
+		<script src="vendor/jquery-validation/jquery.validate.min.js"></script>
+
+		<script src="assets/js/main.js"></script>
+
+		<script src="assets/js/login.js"></script>
+		<script>
+			jQuery(document).ready(function() {
+				Main.init();
+				Login.init();
+			});
+		</script>
+
+	</body>
+	<!-- end: BODY -->
+</html>
